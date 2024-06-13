5eb6f5c3171553eeae0a70db51291871b52d71fe
jasminshaik6866 <jasminshaik6866@gmail.com>
2024-06-13 22:51:40 +0530

    fill the application

---
+++ src/com/basics/PenddingFile.java
@@ -0,0 +1,12 @@
+package com.basics;
+
+public class PenddingFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("fill the pendig part");
+
+	}
+
+}
